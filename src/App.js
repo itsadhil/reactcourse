9a884c50bd98e1484c4e73e9d825be9051398aa5
itsadhil <adhilquazi@gmail.com>
2025-04-10 21:17:55 +0530

    Added footer and updated contents file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Header from './Header';
 import './Header.css'
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handleNameChange = () => {
-    const names = ['bob', 'kevin', 'Dave'];
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-  }
-
   return (
+  <div className="App">
     <Header />
+    <Content />
+    <Footer />
+  </div>
   );
 }
 
