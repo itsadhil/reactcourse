e474ca8c91eb64b7a167ee6f2d188bbdbf34db52
itsadhil <adhilquazi@gmail.com>
2025-04-10 19:41:16 +0530

    Added a Header CSS just to test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import './Header.css'
 
 function App() {
   const handleNameChange = () => {
@@ -9,24 +10,7 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see what I change.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>hello {handleNameChange()}</p>
-      </header>
-      <p>HELLO BRO</p>
-    </div>
+    <Header />
   );
 }
 
