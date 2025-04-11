7fc55b784d99ede013dfb7a38ebe454156f44242
itsadhil <adhilquazi@gmail.com>
2025-04-11 19:01:43 +0530

    Added some functions and learned them

--- src/Content.js
+++ src/Content.js
@@ -9,12 +9,29 @@ const Content = () => {
         alert(`Hello ${handleNameChange()}`);
 }
 
+
+    const handleClick2 = (name) => {
+        console.log(`Hello ${name}`)
+}
+
+    const handleClick3 = (e) => {
+        console.log(e.target)
+}
+
+    const handleClick4 = (e) => {
+        console.log(e.target.innerText)
+}
+
 return (
     <main>
         <p>
         Hello {handleNameChange()}
         </p>
         <button onClick={handleClick}>Click meh</button>
+        <button onClick ={() => handleClick2('Adhil')}>Name Button</button>
+        <button onClick ={(e) => handleClick3(e)}>Target Button</button>
+        <button onClick ={(e) => handleClick4(e)}>InnerText Target Button</button>
+
     </main>
 ) 
 }
