208448a28dbfa8e0ed636b124a8b54a2831cc261
itsadhil <adhilquazi@gmail.com>
2025-04-17 20:10:56 +0530

    fixed some stuff

--- src/Content.js
+++ src/Content.js
@@ -6,59 +6,60 @@ const Content = () => {
         {
             id: 1,
             checked: true,
-            item: "Chicken"
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
         },
         {
             id: 2,
             checked: false,
-            item: "Beef"
+            item: "Item 2"
         },
         {
             id: 3,
             checked: false,
-            item: "Pork"
-        },
-        {
-            id: 4,
-            checked: false,
-            item: "Fish"
-        },
-        {
-            id: 5,
-            checked: false,
-            item: "Vegetarian"
-        },
-        {
-            id: 6,
-            checked: false,
-            item: "Vegan"
+            item: "Item 3"
         }
     ]);
 
     const handleCheck = (id) => {
-        const listitems = items.map((item) => item.id === id ? { ...item,
-        checked: !item.checked } : item);
-        setItems(listitems);
-        }
-    
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
 
-return (
-    <main>
-        <ul>
-            {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <input
-                        type = 'checkbox'
-                        onChange={() => handleCheck(item.id)}
-                        checked = {item.checked}
-                    />
-                    <label>{item.item}</label>
-                    <FaTrashAlt role='button' tabIndex="0"/>
-                </li>
-            ))}
-        </ul>
-    </main>
-) 
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    return (
+        <main>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                                onDoubleClick={() => handleCheck(item.id)}
+                            >{item.item}</label>
+                            <FaTrashAlt
+                                onClick={() => handleDelete(item.id)}
+                                role="button"
+                                tabIndex="0"
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
+        </main>
+    )
 }
 
 export default Content
